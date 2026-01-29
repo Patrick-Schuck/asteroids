0082d04f52a2b07e0d50b415f8dd6c4a095d94c2
Patrick-Schuck <p.schuck-castle@t-online.de>
2026-01-29 18:39:12 +0100

    Gameloop is running!

--- main.py
+++ main.py
@@ -4,10 +4,21 @@ from logger import log_state
 
 def main():
     pygame.init()
-    pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    time = pygame.time.Clock()
+    dt = 0
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+        dt = time.tick(60)
+
 
 if __name__ == "__main__":
     main()
