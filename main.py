e69f2d4f27b3fb95e8e3d51d625e3d649b9b0f7c
Patrick-Schuck <p.schuck-castle@t-online.de>
2026-01-29 19:10:01 +0100

    Drawing the player

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH,PLAYER_RADIUS
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -10,12 +11,14 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     time = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2,PLAYER_RADIUS)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = time.tick(60)
 
